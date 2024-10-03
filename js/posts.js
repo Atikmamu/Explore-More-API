a6f7e53ad4cca4a35dad221a8b5957236e41414f
Atikmamu <robinatik50@gmail.com>
2024-10-03 22:18:23 +0600

    added all file

--- js/posts.js
+++ js/posts.js
@@ -1,34 +1,31 @@
-function loadPosts(){
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => displayPosts(data))
+function loadPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayPosts(data));
 }
 
-
 /**
- * 1. get the container element where you want to add the new elements 
- * 2. create child element 
- * 3. set innerText or innerHTML 
- * 4. appendChild 
+ * 1. get the container element where you want to add the new elements
+ * 2. create child element
+ * 3. set innerText or innerHTML
+ * 4. appendChild
  */
 
-
-function displayPosts(posts){
-    const postContainer = document.getElementById('post-container');
-    for(const post of posts){
-        const postDiv = document.createElement('div');
-        postDiv.classList.add('post')
-        console.log(post);
-        postDiv.innerHTML = `
+function displayPosts(posts) {
+  const postContainer = document.getElementById("post-container");
+  for (const post of posts) {
+    const postDiv = document.createElement("div");
+    postDiv.classList.add("post");
+    console.log(post);
+    postDiv.innerHTML = `
             <h2>User-${post.userId}</h2>
             <h4>Post: ${post.title}</h4>
             <p>Post Description: ${post.body}</p>
        `;
-       postContainer.appendChild(postDiv);
-       
-    }
-        
+    postContainer.appendChild(postDiv);
+  }
 }
 
 loadPosts();
+
 
